cbfe3ef194c2df31d9557901f6595f076bb291b2
Lana Worobec <llama.p0rtk3y@gmail.com>
2018-01-15 21:05:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 const app = "I don't do much."
+
+function addElementToBeginningOfArray(element, array){
+  return [element, ...array];
+}
+
+function destructivelyAddElementToBeginningOfArray(element, array){
+  array.unshift(element);
+  return array;
+}
+
+function addElementToEndOfArray(element, array){
+  return [...array, element];
+}
+
+function destructivelyAppendKitten(element, array){
+  array.push(element);
+  return array;
+}
+
