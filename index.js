c61966802362526210ef5b3c768e3c5e0ee7e6a8
Lana Worobec <llama.p0rtk3y@gmail.com>
2018-01-15 21:21:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,5 +16,8 @@ function addElementToEndOfArray(array, element){
 var kittens = ["Milo", "Otis", "Garfield"];
 
 function destructivelyAppendKitten(array, name){
-  
+  array.push(name);
+  return array;
 }
+
+destructivelyAppendKitten(kittens, name);
